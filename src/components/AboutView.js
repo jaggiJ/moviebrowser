9108602f28f89c0682db509e42b2124f9046400b
jaggiJ <jagged93@gmail.com>
2021-06-16 17:35:21 +0200

    Before making first View

---
+++ src/components/AboutView.js
@@ -0,0 +1,7 @@
+const AboutView = () => {
+    return(
+        <h3>This is AboutView!!!</h3>
+    )
+}
+
+export default AboutView;
---
+++ src/components/Home.js
@@ -0,0 +1,9 @@
+const Home = () => {
+    return(
+      <>
+        Hello world from out HOME !
+      </>
+    );
+}
+
+export default Home;
