b43f3d1544864188cc7ddd1fab0b7d58104e7db1
jaggiJ <jagged93@gmail.com>
2021-07-05 10:53:20 +0200

    added isLoading to MovieView

--- src/components/MovieView.js
+++ src/components/MovieView.js
@@ -4,19 +4,30 @@ import { useState, useEffect } from "react";
 
 const MovieView = () => {
   const { id } = useParams();
-  const [movieDetails, setMovieDetails ] = useState({});
+  const [ movieDetails, setMovieDetails ] = useState({});
+  const [ isLoading, setIsLoading ] = useState(true);
 
   useEffect(() => {
     fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=bc267a3d7338eb52a2fe8359129d2354&language=en-US`)
       .then(response => response.json())
       .then(data => {
         setMovieDetails(data)
+        setIsLoading(false)
       })
   }, [id])
 
+  function renderMovieDetails() {
+    if(isLoading) {
+      return <Hero text='Loading...' />
+    }
+    if(movieDetails) {
+      return <Hero text={movieDetails.original_title} />
+    }
+  }
+
   return (
     <>
-      <Hero text={movieDetails.original_title} />
+      {renderMovieDetails()}
     </>
   );
 };
